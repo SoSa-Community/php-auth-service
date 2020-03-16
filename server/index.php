b6fde3b764ec16e9ba2cb9e261508fb811079cc0
James Mahy <james.mahy@cevo.co.uk>
2020-03-16 21:08:46 +0000

    Initial Ubiquity setup

---
+++ server/index.php
@@ -0,0 +1,7 @@
+<?php
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', __DIR__.DS.'app'.DS);
+$config=include_once ROOT.'config/config.php';
+require_once ROOT.'./../vendor/autoload.php';
+require_once ROOT.'config/services.php';
+\Ubiquity\controllers\Startup::run($config);
