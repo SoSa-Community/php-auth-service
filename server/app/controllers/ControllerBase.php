5defd8d4cd98108872db22fe32bc113bfc9e61ee
James <james.mahy@cevo.co.uk>
2020-04-03 17:30:54 +0100

    Implemented standard approach for JSON / Post data on requests

--- server/app/controllers/ControllerBase.php
+++ server/app/controllers/ControllerBase.php
@@ -9,9 +9,21 @@ use Ubiquity\utils\http\UResponse;
  * ControllerBase.
  **/
 abstract class ControllerBase extends Controller{
+	
 	public function initialize() {
 		UResponse::asJSON();
+		
+		/*  We want to allow both POST bodies and JSON bodies in a unified way
+			so we check to see if the content type header is application/json
+			and then we merge the JSON body with the $_POST variable
+		*/
+		if($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['CONTENT_TYPE'] === 'application/json') {
+		$request = json_decode(trim(file_get_contents("php://input")), true);
+			$_POST = array_merge($_POST, $request);
+		}
+		
 	}
+	
 	public function finalize() {}
 	
 	public static function generateResponse(string $status='failure', $data=null, \Error $error=null){
