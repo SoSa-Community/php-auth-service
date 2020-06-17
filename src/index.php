ee724053b6d31853619f14c56d18bd7b5321ffb0
James <james.mahy@cevo.co.uk>
2020-06-17 19:28:36 +0100

    * Cors (Temporary not for production)

--- src/index.php
+++ src/index.php
@@ -1,4 +1,12 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: *');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+	exit;
+}
+
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', __DIR__.DS.'app'.DS);
 $config=include_once ROOT.'config/config.php';
