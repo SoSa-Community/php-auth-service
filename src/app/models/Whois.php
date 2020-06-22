898fe05f8ebb38cdc6f4e4c38f2a0c2ee5ff6ce5
James <james.mahy@cevo.co.uk>
2020-06-22 19:43:53 +0100

    Implemented WhoisXML API and Cache

---
+++ src/app/models/Whois.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace models;
+
+use Ubiquity\controllers\Startup;
+use Ubiquity\orm\DAO;
+use \Firebase\JWT\JWT;
+/**
+ * @table("name"=>"whois")
+ **/
+class Whois{
+	/**
+	 * @id
+	 */
+	private ?int $id = 0;
+	
+	private string $domain = '';
+	private string $data = '';
+	private int $exists = 0;
+	private string $created = '';
+	private string $updated = '';
+	
+	public function getId(){return $this->id;}
+	public function setId($id){$this->id = $id;}
+	
+	public function getDomain(){return $this->domain;}
+	public function setDomain($domain){$this->domain = $domain;}
+	
+	public function getData(){return $this->domain;}
+	public function setData($data){$this->data = $data;}
+	
+	public function getExists(){return $this->exists;}
+	public function setExists($exists){$this->exists = intval($exists);}
+	
+	public function getCreated(){return $this->created;}
+	public function setCreated($created){$this->created = $created;}
+	
+	public function getUpdated(){return $this->updated;}
+	public function setUpdated($updated){$this->updated = $updated;}
+}
---
+++ src/app/providers/WhoisProvider.php
@@ -0,0 +1,46 @@
+<?php
+namespace providers;
+
+use models\Whois;
+use Ubiquity\controllers\Startup;
+use Ubiquity\orm\DAO;
+
+class WhoisProvider{
+	
+	public static function retrieve($domain){
+		
+		$existingWhois = DAO::getOne(Whois::class, 'domain LIKE ?', false, [$domain]);
+		if(empty($existingWhois)) {
+			
+			$ch = curl_init("https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=" . Startup::$config['providers']['whois']['apiKey'] . "&domainName=" . $domain . "&outputFormat=JSON");
+			
+			$curlOptions[CURLOPT_RETURNTRANSFER] = 1;
+			$curlOptions[CURLOPT_FOLLOWLOCATION] = 1;
+			
+			curl_setopt_array($ch, $curlOptions);
+			$data = curl_exec($ch);
+			
+			if(!empty($data)) {
+				$parsedData = json_decode($data, true);
+				if (!empty($parsedData) && isset($parsedData['WhoisRecord'])) {
+					
+					$whois = new Whois();
+					
+					$whois->setDomain($domain);
+					$whois->setData($data);
+					
+					$whois->setExists((!isset($parsedData['WhoisRecord']['dataError']) || $parsedData['WhoisRecord']['dataError'] !== 'MISSING_WHOIS_DATA'));
+					if (DAO::save($whois)) {
+						return $whois;
+					}
+					
+				}
+			}
+			
+		}else {
+			return $existingWhois;
+		}
+		
+	}
+	
+}
